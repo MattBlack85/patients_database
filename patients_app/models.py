b055866175202cd2669d6a2798c311b78db0dd09
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-20 18:44:54 +0200

    models - comments deleted

--- patients_app/models.py
+++ patients_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Patient(models.Model):
     name = models.CharField(max_length=200)
     pesel = models.IntegerField()
--- visits_app/models.py
+++ visits_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from visits_app.models import Patient
 
-# Create your models here.
+
 class Visit(models.Model):
     patient = models.ForeignKey(Patient)
     date = models.DateField()
