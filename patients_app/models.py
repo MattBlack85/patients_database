74c86293a3e235a91c0592e290b1ba0218731d61
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-22 14:52:25 +0200

    pusho gitignore cambiato (con dbsqlite aggiunto) e patients_app/models.py

--- patients_app/models.py
+++ patients_app/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=200)
     pesel = models.IntegerField()
-    pub_date = models.DateField('patient added on: ')
+    pub_date = models.DateField(auto_now_add=True)
