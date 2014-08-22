4e6bc4ac32d538660b334235163eb4d47fa70099
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-22 16:47:11 +0200

    pesel lungezza

--- patients_app/models.py
+++ patients_app/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(max_length=200)
-    pesel = models.IntegerField()
+    pesel = models.IntegerField(max_length=11)
     pub_date = models.DateField(auto_now_add=True)
+
     def __unicode__(self):
         return self.name, self.pesel
