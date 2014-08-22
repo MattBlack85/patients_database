a82eaa2d16f81d5dc4d4102f197f0ccf8975ae06
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-22 16:48:52 +0200

    blank, null

--- patients_app/models.py
+++ patients_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(max_length=200)
-    pesel = models.IntegerField(max_length=11)
+    pesel = models.IntegerField(max_length=11, blank=True,null=True)
     pub_date = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
