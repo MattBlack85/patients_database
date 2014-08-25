26889edfaf2e016a4461cb1ce9b4619e9f6de485
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-25 12:12:16 +0200

    visits working - but represented just by date of the visit

--- visits_app/models.py
+++ visits_app/models.py
@@ -4,8 +4,8 @@ from patients_app.models import Patient
 
 class Visit(models.Model):
     patient = models.ForeignKey(Patient)
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
     description = models.TextField()
 
     def __unicode__(self):
-        return self.date
+        return str(self.date)
