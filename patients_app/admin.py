48538d5232af5e4ea43b1061af10a0cf8d9f1c65
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 15:01:15 +0200

    filtering and searching by name

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 
 from patients_app.models import Patient
-from visits_app.models import Visit #trial
+from visits_app.models import Visit 
 
-class VisitInline(admin.StackedInline): #trial
-    model=Visit #trial
-    extra=1 #trial
+class VisitInline(admin.StackedInline): 
+    model=Visit 
+    extra=1 
     
 class PatientAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Personal data',{'fields':['pesel']}),
                  (None,{'fields':['name']}),
                                       ]
-    list_display = ('name', 'pesel', 'pub_date')
+    #list_display = ('name', 'pesel')#I don't like the look so I cancel it
+    list_filter = ['name']
+    search_fields=['name']
     inlines = [VisitInline]
     
 admin.site.register(Patient,PatientAdmin)
--- visits_app/admin.py
+++ visits_app/admin.py
@@ -4,6 +4,7 @@ from visits_app.models import Visit
 class VisitAdmin(admin.ModelAdmin):
     fields = ['patient','description']
     list_display = ('description')
+    list_filter = ['date']
     
 #admin.site.register(Visit, VisitAdmin)
 #removed to display visits with patients, not separately
