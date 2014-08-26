86081333caf8c0dea71ec6b0825de6b53be3100f
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 10:30:35 +0200

    customized

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
 from patients_app.models import Patient
-admin.site.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    fields = ['pesel','name']
+admin.site.register(Patient,PatientAdmin)
