d05380456654c801fd5048db3b3c2b020627bc81
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 13:26:00 +0200

    visits connected with patients via INLINE

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -5,11 +5,11 @@ from visits_app.models import Visit #trial
 
 class VisitInline(admin.StackedInline): #trial
     model=Visit #trial
-    extra=3 #trial
+    extra=1 #trial
 class PatientAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Personal data',{'fields':['pesel']}),
                  (None,{'fields':['name']}),
                                       ]
-    
+    inlines = [VisitInline]
 admin.site.register(Patient,PatientAdmin)
