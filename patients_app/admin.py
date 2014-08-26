2db76daa6a5796ae707d48936fe5eb24a4efbe66
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 10:51:41 +0200

    admin.py customized

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from patients_app.models import Patient
 class PatientAdmin(admin.ModelAdmin):
     fields = ['pesel','name']
+    
 admin.site.register(Patient,PatientAdmin)
--- visits_app/admin.py
+++ visits_app/admin.py
@@ -3,4 +3,5 @@ from django.contrib import admin
 from visits_app.models import Visit
 class VisitAdmin(admin.ModelAdmin):
     fields = ['patient','description']
+    
 admin.site.register(Visit, VisitAdmin)
