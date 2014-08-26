5427982cf0a6c90abf4e68e71953eb79cb7c801b
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 10:03:57 +0200

    il sito ha smesso di funzionare :(

--- visits_app/admin.py
+++ visits_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 
 from visits_app.models import Visit
-admin.site.register(Visit)
+Class VisitAdmin(admin.ModelAdmin):
+    fields = ['description','patient']
+admin.site.register(Visit, VisitAdmin)
