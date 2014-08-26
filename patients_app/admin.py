e4f944787fecd5f8197c4a8bc33e2b170de15f37
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 12:25:53 +0200

    a label PERSONAL DATA (ie fieldsets)

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from patients_app.models import Patient
 class PatientAdmin(admin.ModelAdmin):
-    fields = ['pesel','name']
+    fieldsets = [
+        ('Personal data',{'fields':['pesel']}),
+                 (None,{'fields':['name']}),
+                                      ]
     
 admin.site.register(Patient,PatientAdmin)
