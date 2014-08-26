b20ce4c275d14c14498212087fab8cfee69759be
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-26 13:00:40 +0200

    trying to bind visits and patients

--- patients_app/admin.py
+++ patients_app/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 from patients_app.models import Patient
+from visits_app.models import Visit #trial
+
+class VisitInline(admin.StackedInline): #trial
+    model=Visit #trial
+    extra=3 #trial
 class PatientAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Personal data',{'fields':['pesel']}),
--- visits_app/admin.py
+++ visits_app/admin.py
@@ -4,4 +4,5 @@ from visits_app.models import Visit
 class VisitAdmin(admin.ModelAdmin):
     fields = ['patient','description']
     
-admin.site.register(Visit, VisitAdmin)
+#admin.site.register(Visit, VisitAdmin)
+#removed to display visits with patients, not separately
