c1eb5400a184fbf732bd6689aad8a9c995e67288
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-08-27 16:41:44 +0200

    changes in urls and views that should enable user to open an editing_database page, but somehow dont :P

--- patients_app/urls.py
+++ patients_app/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from patients_app import views
 
 urlpatterns = patterns('',
-                       url(r'^$',views.index,name = 'index')
+                       url(r'^$',views.index,name = 'index'),
+                       url(r'^$',views.edit,name = 'editing_database'),
+                       
                        )
--- patients_app/views.py
+++ patients_app/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("you are in index, plain http response")
+    return HttpResponse("you are in index")
+
+def edit(request,name):
+    return HttpResponse("you can edit the database now")
