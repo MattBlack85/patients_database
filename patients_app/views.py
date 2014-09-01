e04c25082ccebd1a16f5edbf9f803782fe79cd73
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-01 17:21:19 +0200

    views INDEX

--- patients_app/views.py
+++ patients_app/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from patients_app.models import Patient
 
 def index(request):
-    return HttpResponse("you are in index")
+    patients_list = Patient.objects.order_by('-name')
+    template = loader.get_template('patients_app/index.html')
+    context = RequestContext(request, {
+        'patients_list': patients_list,
+        })
+    return HttpResponse(template.render(context))
+    
 
 def edit(request, name):
     return HttpResponse("edit patient %s" % name)
