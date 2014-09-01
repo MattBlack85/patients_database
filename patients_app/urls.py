ae7a265e108a42b4ca390597051449806df53cd6
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-01 14:54:52 +0200

    w not s in regex

--- patients_app/urls.py
+++ patients_app/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import patterns, url
 from patients_app import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name = 'index'),
-                       url(r'(?P<name>\d+)/edit/$', views.edit, name = 'edit'),
+                       url(r'^$', views.index, name='index'),
+                       url(r'(?P<name>\w+)/edit/$', views.edit, name='edit'),
                        
                        )
 #r'^blahblah sono i regex
+#d = digits, w = words
 
