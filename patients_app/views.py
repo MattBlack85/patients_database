5eecb0a01f106c7383b6aa26bf4dc05c2ac11b9d
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-01 18:41:30 +0200

    creating EDIT view

--- patients_app/views.py
+++ patients_app/views.py
@@ -2,18 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from patients_app.models import Patient
+from django.http import Http404
+
 
 def index(request):
     patients_list = Patient.objects.order_by('-name')
-    template = loader.get_template('patients_app/index.html')
-    context = RequestContext(request, {
-        'patients_list': patients_list,
-        })
-    return HttpResponse(template.render(context))
-    
+    context = {'patients_list': patients_list}
+    return render(request, 'patients_app/index.html',context)    
 
 def edit(request, name):
-    return HttpResponse("edit patient %s" % name)
+    try:
+        patient = Patient.objects.get(name=name)
+    except:
+        Patient.DoesNotExist:
+            raise Http404
+    return render(request, 'patients_app/edit.html', {'patient':patient})    
+    
+    
 
 
 
