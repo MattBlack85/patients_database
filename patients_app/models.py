28411d8f1f077731dc02a7c0b4dda41f1654366f
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-02 10:11:08 +0200

    additional columns for first and second name

--- patients_app/models.py
+++ patients_app/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 class Patient(models.Model):
     name = models.CharField(max_length=200)
-    pesel = models.IntegerField(max_length=11, blank=True,null=True)
+    first_name = models.CharField(max_length=200)
+    second_name = models.CharField(max_length=200, blank=True, null=True)
+    pesel = models.IntegerField(max_length=11, blank=True, null=True)
     pub_date = models.DateField(auto_now_add=True)
 
     def __unicode__(self):
-        return self.name + " (PESEL: " + str(self.pesel) + ")"
+        if second_name:
+            return self.first_name + " " + self.second_name +" " + self.name + " (PESEL: " + str(self.pesel) + ")"
+        else:
+            return self.first_name + " " + self.name + " (PESEL: " + str(self.pesel) + ")"
