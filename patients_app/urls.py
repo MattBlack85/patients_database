627f5b4f3278541f4c78647ecf966343f1909433
MattBlack85 <promat85@gmail.com>
2014-09-02 12:54:00 +0200

    :nail_care:

--- patients_app/urls.py
+++ patients_app/urls.py
@@ -5,9 +5,6 @@ from patients_app import views
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
                        url(r'(?P<name>\w+)/edit/$', views.edit, name='edit'),
-                       
-                       
                        )
-#r'^blahblah sono i regex
-#d = digits, w = words
-
+# r'^blahblah sono i regex
+# d = digits, w = words
--- visits_app/admin.py
+++ visits_app/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 
-from visits_app.models import Visit
+from .models import Visit
+
+
 class VisitAdmin(admin.ModelAdmin):
-    fields = ['patient','description']
+    fields = ['patient', 'description']
     list_display = ('description')
     list_filter = ['date']
-    
-#admin.site.register(Visit, VisitAdmin)
-#removed to display visits with patients, not separately
+
+
+# admin.site.register(Visit, VisitAdmin)
+# removed to display visits with patients, not separately
