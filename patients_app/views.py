4af3638253b7eae44fea2844c0cea0a9e7d647a4
MattBlack85 <promat85@gmail.com>
2014-09-12 09:29:24 +0200

    Make view create/update objects

--- patients_app/views.py
+++ patients_app/views.py
@@ -10,24 +10,21 @@ from django.http import Http404
 def index(request):
     patients_list = Patient.objects.order_by('-last_name')
     context = {'patients_list' : patients_list}
-    return render(request, 'patients_app/index.html',context)    
+    return render(request, 'patients_app/index.html', context)
 
-def edit(request,pk):
-    print request.POST
-    patient = get_object_or_404(Patient,pk=pk)
-    #visits = Visit.objects.filter(patient=Patient.objects.get(pk=pk))
-    #v_action = "/patients_app/" + str(patient.pk) +"/edit/"
+def edit(request, pk):
+    patient = get_object_or_404(Patient, pk=pk)
     visits = Visit.objects.select_related('patient').filter(patient=patient)
-          
-    v_form = VisitForm(request.POST or None)
-    if v_form.is_valid():
-        description = v_form.save(commit=False)
-        description.post = post
-        description.save
-        
-     
-    return render(request, 'patients_app/edit.html', {'patient':patient, 'visits': visits, 'v_form': v_form})    
-
-
-    
-
+    if request.method == 'POST':
+        if request.POST.get('visit'):  # The visit exists
+            visit_id = request.POST['visit']
+            new_description = request.POST['description']
+            old_visit = patient.visit_set.get(pk=visit_id)
+            old_visit.description = new_description
+            old_visit.save()
+        else:  # We got a new visit
+            v_form = VisitForm(request.POST)
+            if v_form.is_valid():
+                v_form.save()
+            return render(request, 'patients_app/edit.html', {'patient':patient, 'visits': visits, 'v_form': v_form})
+    return render(request, 'patients_app/edit.html', {'patient':patient, 'visits': visits, 'v_form': VisitForm()})
