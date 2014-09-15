53960c9ab2aafb399a8b30ad2ec7e3cd4c92859a
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-15 18:24:17 +0200

    minor changes, problems with displaying of first name persist

--- patients_app/views.py
+++ patients_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import Patient
+from .forms import PatientForm
 from visits_app.models import Visit
 from visits_app.forms import VisitForm
 from django.http import Http404
@@ -21,13 +22,13 @@ def index(request):
             old_patient.second_name = new_second
             old_patient.last_name = new_last
             old_patient.pesel = new_pesel
-            old_patient.save(commit=True)
+            old_patient.save()
         else: #there are no patients
             p_form = PatientForm(request.POST)
             if p_form.is_valid():
-                p_form.save(commit=True)
+                p_form.save()
             return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':p_form})             
-        return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':PatientForm()})
+    return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':PatientForm()})
 
 def edit(request, pk):
     patient = get_object_or_404(Patient, pk=pk)  
@@ -38,10 +39,10 @@ def edit(request, pk):
             new_description = request.POST['description']
             old_visit = patient.visit_set.get(pk=visit_id)
             old_visit.description = new_description
-            old_visit.save(commit=True)
+            old_visit.save()
         else:  # We got a new visit
             v_form = VisitForm(request.POST)
             if v_form.is_valid():
-                v_form.save(commit=True)
+                v_form.save()
             return render(request, 'patients_app/edit.html', {'patient':patient, 'visits': visits, 'v_form': v_form})
     return render(request, 'patients_app/edit.html', {'patient':patient, 'visits': visits, 'v_form': VisitForm()})
