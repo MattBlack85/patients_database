f2fc1d9ea00ab8fdc7592de6d31890a97d673302
Fanny <bezbledna.iluzjonistka@gmail.com>
2014-09-15 17:43:06 +0200

    v_forms and p_forms separated - but still it does not work..

--- patients_app/views.py
+++ patients_app/views.py
@@ -7,20 +7,8 @@ from visits_app.forms import VisitForm
 from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
-
 def index(request):
     patients_list = Patient.objects.order_by('-last_name')
-    context = {'patients_list' : patients_list}
-    if request.method == 'POST':
-        if not request.POST.get('patient'):#the patient does not exist
-            p_form = PatientForm(request.POST)
-            if p_form.is_valid():
-                p_form.save(commit=True)
-            
-    return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':p_form})
-
-def edit(request, pk):
-    patient = get_object_or_404(Patient, pk=pk)
     if request.method == 'POST':
         if request.POST.get('patient'):
             patient_id = request.POST['patient']
@@ -28,13 +16,21 @@ def edit(request, pk):
             new_second = request.POST['second_name']
             new_last = request.POST['last_name']
             new_pesel = request.POST['pesel']
-            old_patient = patient
+            old_patient = patients_list.get(pk=patient_id)
             old_patient.first_name = new_first
             old_patient.second_name = new_second
             old_patient.last_name = new_last
             old_patient.pesel = new_pesel
             old_patient.save(commit=True)
-        
+        else: #there are no patients
+            p_form = PatientForm(request.POST)
+            if p_form.is_valid():
+                p_form.save(commit=True)
+            return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':p_form})             
+        return render(request, 'patients_app/index.html', {'patients_list':patients_list, 'p_form':PatientForm()})
+
+def edit(request, pk):
+    patient = get_object_or_404(Patient, pk=pk)  
     visits = Visit.objects.select_related('patient').filter(patient=patient)
     if request.method == 'POST':
         if request.POST.get('visit'):  # The visit exists
